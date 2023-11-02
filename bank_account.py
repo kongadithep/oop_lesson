6975a6c5242d9c771a5b431c076133e9bd1c3ff8
Adithep KASORNJAN <i_kongza@hotmail.com>
2023-11-02 10:20:52 +0700

    two

--- bank_account.py
+++ bank_account.py
@@ -8,25 +8,26 @@ class AccountDB:
             self.account_database.append(account)
         else:
             print(account, "Duplicated account; nothing to be insert")
-    
+
     def __search_private(self, account_num):
         for i in range(len(self.account_database)):
             if self.account_database[i].account_number == account_num:
                 return i
         return -1
-    
+
     def search_public(self, account_num):
         for account in self.account_database:
             if account.account_number == account_num:
                 return account
         return None
-    
+
     def __str__(self):
         s = ''
         for account in self.account_database:
             s += str(account) + ", "
         return s
-        
+
+
 class Account:
     def __init__(self, num, type, account_name, balance):
         self.account_number = num
@@ -42,7 +43,8 @@ class Account:
             self.balance -= amount
 
     def __str__(self):
-        return '{' + str(self.account_number) + ',' + str(self.type) + ',' + str(self.account_name) + ',' + str(self.balance) + '}'
+        return '{' + str(self.account_number) + ',' + str(self.type) + ',' + str(self.account_name) + ',' + str(
+            self.balance) + '}'
 
 
 account1 = Account("0000", "saving", "David Patterson", 1000)
